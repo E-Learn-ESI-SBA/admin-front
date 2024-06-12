eee44806ef5d0b817254beb13e67797411c13b51
Fendi Mohamed <m.fendi@esi-sba.dz>
2024-06-12 16:11:02 +0100

    restart middlware

--- middleware.ts
+++ middleware.ts
@@ -6,34 +6,34 @@ export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const auth = await getAuth();
 
-  // if (pathname.startsWith("/auth") && !auth.isAuth) {
-  //   if(pathname.match("/auth/logout")){
-  //     return NextResponse.redirect(new URL("/auth", request.url));
-  //   }
+  if (pathname.startsWith("/auth") && !auth.isAuth) {
+    if(pathname.match("/auth/logout")){
+      return NextResponse.redirect(new URL("/auth", request.url));
+    }
 
-  //   return NextResponse.next();
-  // }
+    return NextResponse.next();
+  }
 
-  // if (!auth.isAuth) {
-  //   console.log("match 2")
-  //   return NextResponse.redirect(new URL("/auth", request.url));
-  // }
+  if (!auth.isAuth) {
+    console.log("match 2")
+    return NextResponse.redirect(new URL("/auth", request.url));
+  }
 
-  // if (auth.isAuth && auth.payload?.role != "admin" && !pathname.startsWith("/auth")) {
-  //   console.log("match 3")
-  //   return NextResponse.redirect(new URL("/auth/logout", request.url));
-  // }
+  if (auth.isAuth && auth.payload?.role != "admin" && !pathname.startsWith("/auth")) {
+    console.log("match 3")
+    return NextResponse.redirect(new URL("/auth/logout", request.url));
+  }
 
-  // if(pathname.startsWith("/auth") && !pathname.match("/auth/logout") && auth.isAuth){
-  //   console.log("match 4")
-  //   console.log("Redirecting to /");
-  //   return NextResponse.redirect(new URL("/dashbaord", request.url));
-  // }
+  if(pathname.startsWith("/auth") && !pathname.match("/auth/logout") && auth.isAuth){
+    console.log("match 4")
+    console.log("Redirecting to /");
+    return NextResponse.redirect(new URL("/dashbaord", request.url));
+  }
 
-  // if (pathname.startsWith("/dashboard") && auth.payload?.role != "admin") {
-  //   console.log("match 5")
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
+  if (pathname.startsWith("/dashboard") && auth.payload?.role != "admin") {
+    console.log("match 5")
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 
   NextResponse.next();
   
